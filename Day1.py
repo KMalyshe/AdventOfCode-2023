c0d8ee58403ea680a08bd7861bc59399b9a41955
KMalyshe <kyryl.malyshev@gmail.com>
2023-12-01 10:27:49 +0100

    Part 1 Day 1 complete, Part 2 fixed using enumerate

--- Day1.py
+++ Day1.py
@@ -2,66 +2,25 @@ from InputHelper import readInput
 
 parsedInput = []
 totalsum = 0
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 readInput("Day1.txt", parsedInput)
 
-
 for i in parsedInput:
+    temp = []
     #    PART ONE -----------------------
-    #    temp = []
     #    for element in i:
     #        if (element.isnumeric()):
     #            temp.append(int(element))
     #    totalsum +=  int(str(temp[0]) + str(temp[-1]))
     #    PART ONE ------------------------
 
-    temp = []
-    counter = 0
-    while counter < len(i):
-        element = i[counter]
-        if element.isnumeric():
+    for index, number in enumerate(numbers):
+        i = i.replace(number, number[0] + str(index + 1) + number[-1])
+
+    for element in i:
+        if (element.isnumeric()):
             temp.append(int(element))
-            counter += 1
-            continue
-        match element:
-            case "o":
-                if i[counter:counter+3] == "one":
-                    temp.append(1)
-            case "t":
-                if i[counter:counter+3] == "two":
-                    temp.append(2)
-                elif i[counter:counter+5] == "three":
-                    temp.append(3)
-            case "f":
-                if i[counter:counter+4] == "four":
-                    temp.append(4)
-                elif i[counter:counter+4] == "five":
-                    temp.append(5)
-            case "s":
-                if i[counter:counter+3] == "six":
-                    temp.append(6)
-                elif i[counter:counter+5] == "seven":
-                    temp.append(7)
-            case "e":
-                if i[counter:counter+5] == "eight":
-                    temp.append(8)
-            case "n":
-                if i[counter:counter+4] == "nine":
-                    temp.append(9)
-            case _:
-                counter += 1
-                continue
-        counter += 1
-        continue
     totalsum += int(str(temp[0]) + str(temp[-1]))
-
-
-
-
-
-
-
+    print(i)
 
 print(totalsum)
-
-
-
