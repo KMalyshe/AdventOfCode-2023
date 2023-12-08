e93b316599a89a0d3587d8084fe27c09853322ba
KMalyshe <kyryl.malyshev@gmail.com>
2023-12-08 18:45:58 +0100

    Day 7 Part 2

--- Day7.py
+++ Day7.py
@@ -9,27 +9,41 @@ oak5, oak4, fhouse, oak3, p2, p1, hc = [], [], [], [], [], [], []
 
 
 def gettype(hand):
-    cardset = {hand[0][0], hand[0][1], hand[0][2], hand[0][3], hand[0][4]}
+    # Part 1: solved similarly, minus everything relating to excluding J
+    cardlist = [hand[0][0], hand[0][1], hand[0][2], hand[0][3], hand[0][4]]
+    jcounter = 0
+    while "J" in cardlist:
+        jcounter += 1
+        cardlist.remove("J")
+    cardset = set(cardlist)
     checkdict = {}
 
+    if len(cardset) == 0: return 6
     if len(cardset) == 1:
         return 6  # Five of a kind
     if len(cardset) == 2:
         for card in hand[0]:
+            if card == "J": continue
             if card in checkdict:
                 checkdict[card] += 1
             else:
                 checkdict[card] = 1
-        if 3 in checkdict.values():
-            return 4  # Full House
-        return 5  # Four of a kind
+        if (max(checkdict.values())) + jcounter == 4:
+            return 5 # Four of a kind
+        return 4 # Full House
+        # Part 1:
+        # if 3 in checkdict.values():
+        #     return 4  # Full House
+        # return 5  # Four of a kind
     if len(cardset) == 3:
         for card in hand[0]:
+            if card == "J": continue
             if card in checkdict:
                 checkdict[card] += 1
             else:
                 checkdict[card] = 1
-        if 3 in checkdict.values():
+
+        if (max(checkdict.values())) + jcounter == 3:
             return 3  # Three of a kind
         return 2  # Two Pair
     if len(cardset) == 4:
@@ -57,7 +71,7 @@ def sorthelper(hand):
             handlist.append(int(i))
         else:
             if i == "T": handlist.append(10)
-            if i == "J": handlist.append(11)
+            if i == "J": handlist.append(1) # Part 1: 11
             if i == "Q": handlist.append(12)
             if i == "K": handlist.append(13)
             if i == "A": handlist.append(14)
