66af75dd68821f4092269135784ee5c9a4f2e026
KMalyshe <kyryl.malyshev@gmail.com>
2023-12-27 12:31:20 +0100

    a

--- Testing/Day10.cs
+++ Testing/Day10.cs
@@ -43,14 +43,36 @@ class Day10 {
             curr[2] = 3;
         }
         var stepcounter = 1;
+        List<Tuple<int, int>> vertices = new List<Tuple<int, int>>();
+        vertices.Add((Sy, Sx).ToTuple());
         while (matrix[curr[0]][curr[1]] != 'S')
         {
             stepcounter++;
+            vertices.Add((curr[0], curr[1]).ToTuple());
             var mod1 = curr[0]+getNext(curr,matrix).Item1; var mod2 = curr[1]+getNext(curr,matrix).Item2; var mod3 = getNext(curr,matrix).Item3;
             // Cannot combine into one line, need to assign mod variables otherwise curr gets updated as you go and becomes incorrect.
             curr.SetValue(mod1, 0); curr.SetValue(mod2, 1); curr.SetValue(mod3, 2);
         }
         Console.WriteLine(stepcounter / 2);
+        polygonarea(vertices);
+        static void polygonarea(List<Tuple<int, int>> vertices) 
+        {
+            var num = vertices.Count;
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i<num-1; i++)
+            {
+                sum1 += vertices[i].Item1*vertices[i+1].Item2;
+                sum2 += vertices[i].Item2*vertices[i+1].Item1;
+            }
+
+            sum1 += vertices[num-1].Item1*vertices[0].Item2;
+            sum2 += vertices[0].Item1*vertices[num-1].Item2;
+
+            int area = Math.Abs(sum1-sum2) / 2;
+            Console.WriteLine(area);
+        }
 
 
         static Tuple<int, int, int> getNext(int[] curr, List<List<char>> matrix)
